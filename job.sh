d4da577db58b95f4ce5b49c8182619850a1ee17b
FrancescoEgidioFaggion <150464319+FrancescoEgidioFaggion@users.noreply.github.com>
2024-06-05 15:04:48 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -12,5 +12,5 @@ module load singularity
 export TNPDIR=$HOME/tmp
 mkdir -p $TMPDIR
 
-singularity exec --bind $TMPDIR:$TMPDIR matrix_multiplication.sif bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2" /SwEng/build/main
+singularity exec --bind $TMPDIR:$TMPDIR matrix_multiplication.sif bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 /SwEng/build/main"
 
