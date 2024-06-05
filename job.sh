fa62b0031666be5dcea248386761921143dec29d
FrancescoEgidioFaggion <150464319+FrancescoEgidioFaggion@users.noreply.github.com>
2024-06-05 14:33:05 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=matrix_multiplication
 #SBATCH --output=result.out
 #SBATCH --error=result.err
-#SBATCH --ntasks=4
+#SBATCH --ntasks=2
 
 module load singularity
-singularity exec matrix_multiplication.sif /app/matrix_multiplication matrixA.txt matrixB.txt
+singularity exec matrix_multiplication.sif /SwEng/build/matrix_multiplication matrixA.txt matrixB.txt
 
